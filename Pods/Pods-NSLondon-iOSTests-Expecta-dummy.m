953bcc4f07e5677946bde2a806197031f8ed45c1
Daniel Haight <confidence.designed@gmail.com>
2013-09-08 22:03:01 +0100

    remove Pods from gitignore

---
+++ Pods/Pods-NSLondon-iOSTests-Expecta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NSLondon_iOSTests_Expecta : NSObject
+@end
+@implementation PodsDummy_Pods_NSLondon_iOSTests_Expecta
+@end
---
+++ Pods/Pods-NSLondon-iOSTests-Specta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NSLondon_iOSTests_Specta : NSObject
+@end
+@implementation PodsDummy_Pods_NSLondon_iOSTests_Specta
+@end
